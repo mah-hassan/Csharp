22774834d81d2bff7b780278596c5f6291f4587e
Mahmoud Hassan <mahmoudbus7@gmail.com>
2023-07-13 18:07:56 +0200

    Extension Methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime date = DateTime.Now; // device time
+        DateTimeOffset dateOffset = DateTimeOffset.Now; // device time + or - diffrence from utc time
+        Console.WriteLine("DateTime → Device date: " + date);
+        Console.WriteLine("DateTimeOffsetNow: " + dateOffset);
+        DateTimeOffset dateOffsetUtcNow = DateTimeOffset.UtcNow; // utc time
+        Console.WriteLine("DateTimeOffsetUtcNow: " + dateOffsetUtcNow);
+
+
+        Console.WriteLine($"is {date.DayOfWeek} Week End: {DateHelper.isWeekEnd(date)}"); // instance method
+        Console.WriteLine($"is {date.DayOfWeek} Week End: {date.isWeekEnd()}"); // extention method
+        Console.WriteLine($"is {date.DayOfWeek} Week Day: {DateHelper.isWeekDay(date)}"); // instance method
+        Console.WriteLine($"is {date.DayOfWeek} Week Day: {date.isWeekDay()}"); // extention method
+
+    }
+}
+
+static class DateHelper
+{
+    public static bool isWeekEnd(this DateTime date)
+        => date.DayOfWeek == DayOfWeek.Friday || date.DayOfWeek == DayOfWeek.Saturday;
+    
+    public static bool isWeekDay(this DateTime date) => !isWeekEnd(date);
+}
