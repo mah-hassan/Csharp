100cd88690c886f02115ba67acb555988610e5cc
Mahmoud Hassan <mahmoudbus7@gmail.com>
2023-07-16 14:21:03 +0200

    Dictionary

---
+++ Dictionary/Employee.cs
@@ -0,0 +1,30 @@
+﻿internal class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Rule { get; set; }
+    public int? ReportTo { get; set; }
+
+    public override string ToString()
+    {
+        return $"\n\t\tID: {Id} \n\t\tName: {Name} \n\t\tRule: {Rule} \n\t\tReportTo: {ReportTo} \n";
+    }
+    public override int GetHashCode()
+    {
+        int hash = 13;
+        hash = (hash * 7) + Id.GetHashCode();
+        hash = (hash * 7) + Name.GetHashCode();
+        hash = (hash * 7) + Rule.GetHashCode();
+        hash = (hash * 7) + ReportTo.GetHashCode();
+        return hash;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        Employee? emp = obj as Employee;
+        return emp is { } && this.Id == emp.Id
+            && this.Name == emp.Name
+            && this.Rule == emp.Rule
+            && this.ReportTo == emp.ReportTo;
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee{Id = 1 , Name = "John", Rule = "CEO", ReportTo = null},
+            new Employee{Id = 2 , Name = "Jane", Rule = "COO", ReportTo = 1},
+            new Employee{Id = 3 , Name = "Bob", Rule = "CFO", ReportTo = 1},
+            new Employee{Id = 4 , Name = "Alice", Rule = "CTO", ReportTo = 1},
+            new Employee{Id = 5 , Name = "Tom", Rule = "Manager", ReportTo = 2},
+            new Employee{Id = 6 , Name = "Jerry", Rule = "Manager", ReportTo = 2},
+            new Employee{Id = 6 , Name = "Jerry", Rule = "Manager", ReportTo = 2},
+            new Employee{Id = 8 , Name = "Sara", Rule = "Manager", ReportTo = 3},
+            new Employee{Id = 9 , Name = "David", Rule = "Manager", ReportTo = 4},
+            new Employee{Id = 10, Name = "Emily", Rule = "Manager", ReportTo = 4},
+            new Employee{Id = 11, Name = "Alex", Rule = "Team Lead", ReportTo = 5},
+            new Employee{Id = 12, Name = "Sophie", Rule = "Team Lead", ReportTo = 5},
+            new Employee{Id = 13, Name = "Max", Rule = "Team Lead", ReportTo = 6},
+            new Employee{Id = 14, Name = "Lily", Rule = "Team Lead", ReportTo = 6},
+            new Employee{Id = 15, Name = "Chris", Rule = "Team Lead", ReportTo = 7},
+            new Employee{Id = 16, Name = "Alice", Rule = "Backend Developer", ReportTo = 10},
+            new Employee{Id = 17, Name = "Bob", Rule = "Frontend Developer", ReportTo = 11},
+            new Employee{Id = 18, Name = "Ali", Rule = "Backend Developer", ReportTo = 10},
+            new Employee{Id = 19, Name = "Ahmed", Rule = "Frontend Developer", ReportTo = 11},
+
+        };
+
+        var managers = employees.ToLookup(x => x.Rule).ToDictionary(x => x.Key, x => x.ToList());
+
+        foreach (var item in managers)
+        {
+
+            Console.WriteLine($"\n{item.Key}: ");
+            foreach (var employee in item.Value)
+            {
+                Console.Write(employee);
+            }
+        }
+    }
+}
